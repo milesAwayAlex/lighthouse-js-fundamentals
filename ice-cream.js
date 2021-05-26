8f01d1d72e9b6f47065f932533c3fff5f3348ba3
Alex Mozgovoy <milesAwayAlex@gmail.com>
2021-05-26 01:34:48 +0000

    Complete the ice-cream

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+// Do not modify these first two lines
+const iceCreamFlavours = [
+  'chocolate',
+  'vanilla',
+  'cookies and cream',
+  'rocky road',
+  'strawberry',
+];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+const newIceCream = iceCreamFlavours.concat('root beer');
+console.log(newIceCream);
+
+console.log(newIceCream[0]);
+console.log(newIceCream[newIceCream.length - 1]);
+console.log(newIceCream.length);
