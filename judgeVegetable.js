f453aca4222b59e63f74fb46bbed9862e98050df
Alex Mozgovoy <milesAwayAlex@gmail.com>
2021-05-26 05:01:47 +0000

    Complete judgeVegetable

---
+++ judgeVegetable.js
@@ -0,0 +1,25 @@
+const judgeVegetable = (vegetables, metric) => {
+  const arrangedVeggies = [...vegetables].sort((a, b) => b[metric] - a[metric]);
+  return arrangedVeggies[0].submitter;
+};
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5,
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8,
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3,
+  },
+];
+const metric = 'redness';
+
+console.log(judgeVegetable(vegetables, metric));
